888395ed854b8d279a4a83d33dbb3aa93d353183
Doğan Şengül <60586840+dogansengul@users.noreply.github.com>
2025-05-19 06:31:43 +0300

    updated project structure, duplicate files and empty files removed.

--- gradreq/src/features/auth/components/types/index.ts
+++
@@ -1,8 +0,0 @@
-export enum UserType {
-  STUDENT = "student",
-  ADVISOR = "advisor",
-  SECRETARY = "secretary",
-  DEANS_OFFICE = "deans_office",
-  STUDENT_AFFAIRS = "student_affairs",
-  ADMIN = "admin",
-}
--- gradreq/src/pages/index.d.ts
+++
@@ -1,2 +0,0 @@
-declare module "../pages/LoginPage";
-declare module "../pages/StudentDashboard";
